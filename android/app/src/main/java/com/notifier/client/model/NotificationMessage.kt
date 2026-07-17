2432c0e1132a8b0ac5df7e8070c95bb7cd52247c
FabrichJean <153196686+FabrichJean@users.noreply.github.com>
2026-07-17 15:04:10 +0000

    Add notification message model and Android .gitignore
    Define NotificationMessage as the data model deserialized from incoming WebSocket payloads, and add a .gitignore for the Android module build artifacts.

---
+++ android/app/src/main/java/com/notifier/client/model/NotificationMessage.kt
@@ -0,0 +1,35 @@
+package com.notifier.client.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class AlarmConfig(
+    val fullScreen: Boolean = true,
+    val sound: String = "default",
+    val loop: Boolean = true,
+    val vibrate: Boolean = true,
+    val snoozeMinutes: Int? = null,
+)
+
+@Serializable
+data class NotificationData(
+    val id: String,
+    val title: String,
+    val body: String,
+    val type: String,
+    val alarm: AlarmConfig? = null,
+    val source: String,
+    val createdAt: String,
+)
+
+@Serializable
+data class NotificationEnvelope(
+    val event: String,
+    val data: NotificationData,
+)
+
+@Serializable
+data class AckMessage(
+    val event: String = "ack",
+    val id: String,
+)
